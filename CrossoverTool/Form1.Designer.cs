b0847ab9be0fdf17416ed8596869ef36dbfac0e0
V <V@LAPTOP-66GCD269>
2019-01-23 11:16:01 +0800

    update

--- CrossoverTool/Form1.Designer.cs
+++ CrossoverTool/Form1.Designer.cs
@@ -29,17 +29,23 @@
         private void InitializeComponent()
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
@@ -48,33 +54,18 @@
             // 
             this.splitContainer1.Panel1.Controls.Add(this.button4);
             this.splitContainer1.Panel1.Controls.Add(this.button3);
-            this.splitContainer1.Panel1.Controls.Add(this.button2);
             this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
             this.splitContainer1.Size = new System.Drawing.Size(1295, 711);
             this.splitContainer1.SplitterDistance = 241;
             this.splitContainer1.TabIndex = 0;
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(77, 66);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "转换文件";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(77, 227);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "默认保存";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(77, 115);
+            this.button3.Location = new System.Drawing.Point(77, 179);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -82,14 +73,49 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "转换文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(77, 274);
+            this.button4.Location = new System.Drawing.Point(51, 270);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(140, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "选择保存";
+            this.button4.Text = "转换文件夹下所有文件\r\n";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Size = new System.Drawing.Size(1050, 711);
+            this.splitContainer2.SplitterDistance = 528;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(84, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 27);
+            this.label1.TabIndex = 0;
             // 
             // Form1
             // 
@@ -100,8 +126,13 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -109,10 +140,11 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CrossoverTool/Form1.cs
+++ CrossoverTool/Form1.cs
@@ -17,12 +17,17 @@ namespace CrossoverTool
 {
     public partial class Form1 : Form
     {
+        System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
         private VIAHalImageDisplay display;
         private string path = System.IO.Directory.GetCurrentDirectory() + "\\img";
         public Form1()
         {
             InitializeComponent();
             display = new VIAHalImageDisplay();
+            myTimer.Tick += new EventHandler(timer1_Tick);
+            myTimer.Enabled = true;
+            myTimer.Interval = 100;
+            myTimer.Start();
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -54,15 +59,13 @@ namespace CrossoverTool
                     DirectoryInfo directory1 = new DirectoryInfo(path);
                     if (!directory1.Exists)//不存在
                         directory1.Create();
-
-                    // string[] safeNames = finder.SafeFileNames;
-                    //string[] names = finder.FileNames;
                     {
                         int indexA= 0;
                         for (int i = 0; i < names.Length; i++)
                         {
                             indexA++;
                             HImage image = null;
+                            labal = names[i];
                             ConvertToHalImg.ConvertFunc(out image, names[i]);
                             //HalPosition index = display.GetIndex(safeNames[i]);
                             //if (HalPosition.None != index)
@@ -80,5 +83,107 @@ namespace CrossoverTool
                 thread.Start();
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            OpenFileDialog finder = new OpenFileDialog();
+            finder.Filter= "(点云文件)|*.asc;*.ply";
+            finder.Multiselect = true;
+            if (DialogResult.OK==finder.ShowDialog())
+            {
+                string pathdir = finder.FileName;
+                string[] filename = pathdir.Split('.');
+                string[] dirarr = filename[0].Split('\\');
+                int dirindex = dirarr.Length-1;
+                path = @"D:\" + dirarr[dirindex];
+                DirectoryInfo DirInfo = new DirectoryInfo(path);
+                if (!DirInfo.Exists)
+                    DirInfo.Create();
+                Thread thread = new Thread(() =>
+                {
+                    int index = 0;
+                    string[] names = finder.FileNames;
+                    for (int i = 0; i <names.Length; i++)
+                    {
+                        index++;
+                        HImage img;
+                        labal = names[i];
+                        ConvertToHalImg.ConvertFunc(out img, names[i]);
+                        img.WriteImage("tiff", 0,path+"\\"+index+".tiff");
+                    }
+                    MessageBox.Show("sucess" + filename[0]);
+                });
+                thread.Start();              
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.ShowNewFolderButton = true;
+            string parhp = @"D:\图片\1.21重复性32组原始数据\1.21\产品";
+            fb.SelectedPath = parhp;
+            if (DialogResult.OK == fb.ShowDialog())
+            {
+                string pathdir = fb.SelectedPath;
+                DirectoryInfo dir = new DirectoryInfo(pathdir);
+                Thread thread = new Thread(() =>
+                {
+                    FindFile(dir);
+                    MessageBox.Show("Sucess");
+                });
+                thread.Start();
+            }            
+            
+         }
+      static   string parhp = @"D:\图片\1.21重复性32组原始数据\1.21\产品";
+       static  int Dirindex = 0;
+        static string  indexA = "1";
+        static string labal = string.Empty;
+        static void FindFile(DirectoryInfo di)
+        {
+
+                FileInfo[] fis = di.GetFiles();
+                int index = 0;
+                parhp = @"D:\转换图片" + "\\" + indexA;
+                DirectoryInfo dir = new DirectoryInfo(parhp);
+                if (!dir.Exists)
+                    dir.Create();
+                for (int i = 0; i < fis.Length; i++)
+                {
+                    index++;
+                    HImage img;
+                    labal = fis[i].FullName;
+                    ConvertToHalImg.ConvertFunc(out img, fis[i].FullName);
+                    img.WriteImage("tiff", 0, parhp + "\\" + index + ".tiff");
+                    Console.WriteLine("文件：" + fis[i].FullName);
+                }
+                DirectoryInfo[] dis = di.GetDirectories();
+
+                for (; Dirindex < dis.Length; Dirindex++)
+                {
+                    Console.WriteLine("目录：" + dis[Dirindex].FullName);
+                    if (Dirindex != 0)
+                    {
+                        indexA = dis[Dirindex].FullName;
+                        string[] indexaa = indexA.Split('\\');
+                        indexA = indexaa[indexaa.Length - 1];
+                    }
+                    else
+                    {
+                        indexA = "1";
+                    }
+                    FindFile(dis[Dirindex]);
+                }           
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.label1.Text = "正在转换的图片："+labal;
+        }
+
     }
 }
